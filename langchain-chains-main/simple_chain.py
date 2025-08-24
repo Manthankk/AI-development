696496ba0a715274ebdf66d09359f07394dfe815
admin <manthankhamkar9@gmail.com>
2025-08-24 22:13:59 +0530

    types of chains

---
+++ langchain-chains-main/simple_chain.py
@@ -0,0 +1,46 @@
+# from langchain_openai import ChatOpenAI
+# from dotenv import load_dotenv
+# from langchain_core.prompts import PromptTemplate
+# from langchain_core.output_parsers import StrOutputParser
+
+# load_dotenv()
+
+# prompt = PromptTemplate(
+#     template='Generate 5 interesting facts about {topic}',
+#     input_variables=['topic']
+# )
+
+# model = ChatOpenAI()
+
+# parser = StrOutputParser()
+
+# chain = prompt | model | parser
+
+# result = chain.invoke({'topic':'cricket'})
+
+# print(result)
+
+# chain.get_graph().print_ascii()
+
+
+
+
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate, prompt
+from langchain_core.output_parsers import StrOutputParser
+
+load_dotenv()
+prompt=PromptTemplate(
+    template='Generate 5 intereting facts about{topic}',
+    input_variables=['topic']
+)
+model = ChatGoogleGenerativeAI(model="gemini-1.5-flash")
+
+parser=StrOutputParser()
+
+chain=prompt | model | parser 
+result=chain.invoke({'topic':'cricket'})
+print(result)
+
+chain.get_graph().print_ascii()
